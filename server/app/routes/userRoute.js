e7abfe7e5d971769ea9116e3a79e4786ee21b66c
Atakikoo <atakikoo@gmail.com>
2015-11-19 15:49:00 +0100

    crude create user

--- server/app/routes/userRoute.js
+++ server/app/routes/userRoute.js
@@ -6,13 +6,13 @@ module.exports = function(app) {
   /*-----USER CRUD-----*/
 
     // get all users
-    app.get('/uberParty/users', function(req, res) {
-    User.find(function(err, users) {
-      if (err)
-        res.send(err);
-      res.json(users);
-    });
-  });
+  //   app.get('/uberParty/users', function(req, res) {
+  //   User.find(function(err, users) {
+  //     if (err)
+  //       res.send(err);
+  //     res.json(users);
+  //   });
+  // });
 
   // create user
   app.post('/uberParty/users', function(req, res) {
@@ -27,40 +27,40 @@ module.exports = function(app) {
     });
   });
 
-  // delete user
-  app.delete('/uberParty/users/:user_id', function(req, res) {
-    User.remove({
-      _id: req.params.user_id
-    }, function(err, user) {
-      if(err)
-        res.send(err);
-      res.status(200).end();
-    })
-  });
+//   // delete user
+//   app.delete('/uberParty/users/:user_id', function(req, res) {
+//     User.remove({
+//       _id: req.params.user_id
+//     }, function(err, user) {
+//       if(err)
+//         res.send(err);
+//       res.status(200).end();
+//     })
+//   });
 
-  // update user
-  app.put('/api/users/:user_id', function(req, res){
-    User.update({
-        _id: req.params.user_id
-    }, {$set: {speudo: req.body.username,
-              password: req.body.password},
-        $inc: {__v: 1}
-    }, {overwrite: true}, function(err){
-      if (err)
-        res.send(err);
-      res.status(200).end();
-    })
-  });
+//   // update user
+//   app.put('/api/users/:user_id', function(req, res){
+//     User.update({
+//         _id: req.params.user_id
+//     }, {$set: {speudo: req.body.username,
+//               password: req.body.password},
+//         $inc: {__v: 1}
+//     }, {overwrite: true}, function(err){
+//       if (err)
+//         res.send(err);
+//       res.status(200).end();
+//     })
+//   });
 
-//checkUser
-  app.post('/api/login', function(req, res) {
-    User.find({
-      username: req.body.username,
-      password: req.body.pwd
-    }, function(err, user){
-      if (err || user.length == 0)
-          res.status(404).end();
-      res.status(200).end();
-    })
-  });
+// //checkUser
+//   app.post('/api/login', function(req, res) {
+//     User.find({
+//       username: req.body.username,
+//       password: req.body.pwd
+//     }, function(err, user){
+//       if (err || user.length == 0)
+//           res.status(404).end();
+//       res.status(200).end();
+//     })
+//   });
 }
